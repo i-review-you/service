3cec3a79d3e6f791e248ea11cc18f151124def3c
심진석 <jinseok@simjinseok.com>
2024-09-21 12:04:16 +0900

    레이아웃 수정 및 폰트 적용

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,10 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT@2/fonts/variable/woff2/SUIT-Variable.css"
+          rel="stylesheet"/>
+      </head>
       <body>
         {children}
       </body>
