743fbec546d38e7c1742c028d270b3d47f57da47
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-09-23 21:24:04 +0900

    UI 컴포넌트 추가 : Input, Button 등

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#17B16B",
+      },
+    },
   },
   plugins: [],
 };
