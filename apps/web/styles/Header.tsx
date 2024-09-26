2f4b26915810d73393a93da21717b5ace2d1c4f8
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-09-26 18:09:17 +0900

    header UI 수정

--- apps/web/styles/Header.tsx
+++ apps/web/styles/Header.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
-  return <div className="h-20 bg-[#D9D9D9]">Header</div>;
+  return (
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 max-w-[768px] w-full z-50 flex justify-between bg-white border-b border-gray-200 px-8 pt-16 pb-2">
+      <Link href={"/"}>
+        <Image
+          src="/images/logo-small.svg"
+          alt="I Review U"
+          width={131}
+          height={100}
+          objectFit="cover"
+        />
+      </Link>
+      <Link href={"/user"}>
+        <UserCircleIcon className="size-10" />
+      </Link>
+    </header>
+  );
 }
