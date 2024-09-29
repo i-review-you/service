2ea5bdfa85c0254fbdb7cbbbf0ba6e488ab3e84f
심진석 <jinseok@simjinseok.com>
2024-09-29 11:27:27 +0900

    빌드 테스트

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,22 @@
-import {NestFactory} from '@nestjs/core';
-import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        cors: {
-            origin: 'http://localhost:5173',
-        }
-    });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:5173',
+    },
+  });
 
-    const config = new DocumentBuilder()
-        .setTitle('아이리뷰유 API')
-        .addBearerAuth()
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('아이리뷰유 API')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+  await app.listen(3000);
 }
 
 bootstrap();
