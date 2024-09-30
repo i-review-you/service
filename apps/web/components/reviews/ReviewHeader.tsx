9df6f1db80a28a0f34d9344055be46a98125113c
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-09-30 19:28:15 +0900

    리뷰 페이지 모달 컴포넌트 적용

---
+++ apps/web/components/reviews/ReviewHeader.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import {
+  EllipsisHorizontalIcon,
+  StarIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+import { StarIcon as OutlineStarIcon } from "@heroicons/react/24/outline";
+
+import Modal from "../ui/Modal";
+import ReviewEditModal from "./ReviewEditModal";
+
+export default function ReviewHeader({
+  userId,
+  rating,
+  reviewId,
+}: {
+  userId: string;
+  rating: number;
+  reviewId: number;
+}) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="flex justify-between items-center pb-2">
+      <div className="flex items-center gap-2">
+        <Link
+          href={`/user/${userId}`}
+          className="flex items-center gap-1 cursor-pointer"
+        >
+          <UserCircleIcon className="size-8" />
+          <span>{userId}</span>
+        </Link>
+        <div className="flex">
+          {Array.from({ length: rating })
+            .fill(0)
+            .map((_, index) => (
+              <StarIcon key={index} className="size-5 text-primary" />
+            ))}
+          {Array.from({ length: 5 - rating })
+            .fill(0)
+            .map((_, index) => (
+              <OutlineStarIcon key={index} className="size-5 text-primary" />
+            ))}
+        </div>
+      </div>
+      <div className="relative">
+        {/* 내 리뷰때만 보이게 */}
+        <EllipsisHorizontalIcon
+          className="size-6 cursor-pointer"
+          onClick={toggleModal}
+        />
+        {isModalOpen && (
+          <Modal toggleModal={toggleModal}>
+            <ReviewEditModal reviewId={reviewId} />
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
+}
