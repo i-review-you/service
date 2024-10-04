b245d86c06606665cbb207b19f6c40b9df716f63
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-10-04 18:08:31 +0900

    Button컴포넌트 수정

--- packages/react-components/src/button/index.tsx
+++ packages/react-components/src/button/index.tsx
@@ -1,32 +1,34 @@
-import React, {type ComponentPropsWithoutRef} from 'react';
-import {clsx} from 'clsx';
+import React, { type ComponentPropsWithoutRef } from 'react';
+import { clsx } from 'clsx';
 
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
-    size?: "medium" | "small";
-    scheme?: "primary" | "secondary" | "active" | "inactive";
+  size?: 'large' | 'medium' | 'small';
+  scheme?: 'primary' | 'secondary' | 'active' | 'inactive';
 }
 
 export function Button({
-                           className,
-                           type = "button",
-                           size = "medium",
-                           scheme = "primary",
-                           ...props
-                       }: ButtonProps) {
-    return (
-        <button
-            type={type}
-            className={clsx(
-                className,
-                'font-bold px-5 rounded-[10px]',
-                size === 'medium' && 'py-4 text-lg',
-                size === 'small' && 'py-2',
-                scheme === 'primary' && 'bg-primary text-white',
-                scheme === 'secondary' && 'bg-black text-white',
-                scheme === 'active' && 'bg-white text-primary border border-primary',
-                scheme === 'inactive' && 'bg-white text-gray-400 border border-gray-300'
-            )}
-            {...props}
-        />
-    );
-};
+  className,
+  type = 'button',
+  size = 'medium',
+  scheme = 'primary',
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={clsx(
+        className,
+        'font-bold',
+        size === 'large' &&
+          'fixed z-50 bottom-0 left-1/2 transform -translate-x-1/2 w-[768px] px-6 pt-4 pb-12 text-xl',
+        size === 'medium' && 'w-full rounded-[10px] py-4 text-lg',
+        size === 'small' && 'w-[150px] rounded-[10px] py-2',
+        scheme === 'primary' && 'bg-primary text-white',
+        scheme === 'secondary' && 'bg-black text-white',
+        scheme === 'active' && 'bg-white text-primary border border-primary',
+        scheme === 'inactive' && 'bg-white text-gray-400 border border-gray-300'
+      )}
+      {...props}
+    />
+  );
+}
