dd9d2aa0763bc0135eba209e67c38b60f99018aa
심진석 <jinseok@simjinseok.com>
2024-10-06 22:45:45 +0900

    레이아웃 개선

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,14 +5,14 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className="h-full">
       <head>
         <link
           href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT@2/fonts/variable/woff2/SUIT-Variable.css"
           rel="stylesheet"
         />
       </head>
-      <body>
+      <body className="h-full">
         {children}
       </body>
       {/* <GoogleAnalytics gaId="필요함" /> */}
